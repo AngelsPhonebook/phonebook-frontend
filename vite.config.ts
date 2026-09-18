13f838c0dfa632eb84de8dfdac7f4d3cf62c9354
Sergey Denisov <sergdenisov@ozon.ru>
2026-09-18 23:09:51 +0300

    Правит конфиг

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: process.env.HOST ?? 'localhost',
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  }
 });
